74327d3fdb8b2181d15cdcc4ef4be271800fed7f
KUSUMA <85622787+kusuma895@users.noreply.github.com>
2023-07-28 20:47:58 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,54 @@
+ReactDOM.render(
+<div id='div'>
+<h1>APJ ABDUL KALAM</h1>
+<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/A._P._J._Abdul_Kalam.jpg/330px-A._P._J._Abdul_Kalam.jpg"/>
+<h3 id='h3'>About</h3>
+<div id='para1'>
+<p>Avul Pakir Jainulabdeen Abdul Kalam BR was an Indian aerospace scientist and statesman who served as the 11th president of India from</p>
+<p> 2002 to 2007.He was born and raised in Rameswaram, Tamil Nadu and studied physics and aerospace engineeringAfter graduating from the </p>
+<p>Madras Institute of Technology in 1960, Kalam joined the Aeronautical Development Establishment of the Defence Research and Development</p>
+<p>Organisation (by Press Information Bureau, Government of India) as a scientist after becoming a member of the Defence Research & Development</p>
+<p>Service (DRDS). He started his career by designing a small hovercraft, but remained unconvinced by his choice of a job at DRDO.[26] Kalam</p>
+<p>joined the INCOSPAR, working under Vikram Sarabhai, the renowned space scientist.[13] He was interviewed and recruited into ISRO by </p>
+<p>H. G. S. Murthy, the first Director of Thumba Equatorial Rocket Launching Station (TERLS).[27] In 1969, Kalam was transferred to the </p>
+<p>Indian Space Research Organisation (ISRO) where he was the project director of India's first Satellite Launch Vehicle (SLV-III) which </p>
+<p>successfully deployed the Rohini satellite in near-earth orbit in July 1980; Kalam had first started work on an expandable rocket project</p>
+<p>independently at DRDO in 1965.[1] In 1969, Kalam received the government's approval and expanded the programme to include more engineers.</p>
+<p>Kalam was invited by Raja Ramanna to witness the country's first nuclear test Smiling Buddha as the representative of TBRL, even though </p>
+<p>he had not participated in its development. In the 1970s, Kalam also directed two projects, Project Devil and Project Valiant, which </p>
+<p>sought to develop ballistic missiles from the technology of the successful SLV programme.[29] Despite the disapproval of the Union </p>
+<p>Cabinet, Prime Minister Indira Gandhi allotted secret funds for these aerospace projects through her discretionary powers under </p>
+<p>Kalam's directorship.[29] Kalam played an integral role convincing the Union Cabinet to conceal the true nature of these classified </p>
+<p>aerospace projects.[29] His research and educational leadership brought him great laurels and prestige in the 1980s, which prompted </p>
+<p>the government to initiate an advanced missile programme under his directorship.[29] Kalam and Dr V S Arunachalam, metallurgist and </p>
+<p>scientific adviser to the Defence Minister, worked on the suggestion by the then Defence Minister, R. Venkataraman on a proposal for </p>
+<p>simultaneous development of a quiver of missiles instead of taking planned missiles one after another.[30] R Venkatraman was instrumental</p>
+<p>in getting the cabinet approval for allocating ₹ 3.88 billion for the mission, named Integrated Guided Missile Development Programme (IGMDP) </p>
+<p>and appointed Kalam as the chief executive.[30] Kalam played a major part in developing many missiles under the mission including Agni, </p>
+<p>an intermediate range ballistic missile and Prithvi, the tactical surface-to-surface missile, although the projects have been criticised </p>
+<p>
+    for mismanagement and cost and time overruns.[30][31].Kalam served as the Chief Scientific Adviser to the Prime Minister and Secretary of the Defence Research and Development Organisation from July 1992 to December 1999. The Pokhran-II nuclear tests were conducted during this period in which he played an intensive political and technological role. Kalam served as the Chief Project Coordinator, along with Rajagopala Chidambaram, during the testing phase.[11][32] Media coverage of Kalam during this period made him the country's best known nuclear scientist.[33] However, the director of the site test, K Santhanam, said that the thermonuclear bomb had been a "fizzle" and criticised Kalam for issuing an incorrect report.[34] Both Kalam and Chidambaram dismissed the claims.[35]
+
+In 1998, along with cardiologist Soma Raju, Kalam developed a low cost coronary stent, named the "Kalam-Raju Stent".[36][37] In 2012, the duo designed a rugged tablet computer for health care in rural areas, which was named the "Kalam-Raju Tablet".[38]</p>
+</div>
+<div id='para2'>
+<h3>Born</h3>
+<p>15 October 1931, Rameswaram</p>
+<h3>Died</h3>
+<p>27 July 2015, Shillong</p>
+<h3>Awards</h3>
+<p>Padma Bhushan</p>
+<p>Padma Vibhushan</p>
+<p>Bharat Ratna</p>
+<p>Indira Gandhi Award for National Integration</p>
+<p>Veer Savarkar Award</p>
+<p>SASTRA Ramanujan Prize</p>
+<h3>Parents</h3>
+<p>Jainulabiddin Marakayar, Ashiamma Jainulabiddin</p>
+<h3>Full Name</h3>
+<p>Avul Pakir Jainulabdeen Abdul Kalam</p>
+<h3>Profession</h3>
+<p>Aerospace Scientist, Author</p>
+
+</div>
+</div>,document.getElementById("content"));
